a8861d914be59a61b1037d93e13efb0f71884dbe
stephaniemaf <farrell.s.ava@gmail.com>
2024-03-06 17:29:41 +0000

    abiltiy to searcg by name or description

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
+
 
 # Create your views here.
 
@@ -7,9 +10,22 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "you didnt enter any valid search items ")
+                return redirect(reverse('products'))
+
+        """ enables user to search in name OR*- description"""
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query
     }
 
     return render(request, 'products/products.html', context)
