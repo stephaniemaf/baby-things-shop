361dc80566e00be3675f699e7ec75c5a96391d53
stephaniemaf <farrell.s.ava@gmail.com>
2024-03-07 17:52:25 +0000

    updated context.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,25 @@
 from django.conf import settings
 from decimal import Decimal
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 """ making context availble threw entire aplication"""
 
 def bag_contents(request):
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     delivery = total * Decimal(settings.STARNDARD_DELIVERY_PERCENTAGE / 100)
     grand_total = delivery + total 
