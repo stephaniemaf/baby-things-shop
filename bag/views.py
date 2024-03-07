3b7d1839d54b0bc21c829fd6b0113bf2b0d0abf9
stephaniemaf <farrell.s.ava@gmail.com>
2024-03-07 18:57:08 +0000

    added remove from bag button but does not work yet

--- bag/views.py
+++ bag/views.py
@@ -23,3 +23,17 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def remove_from_bag(request, item_id):
+    """ Add aproduct to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] -= quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
