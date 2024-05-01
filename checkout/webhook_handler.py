da7addee529beae7e583e618f4cf15b84d25dd6e
stephaniemaf <farrell.s.ava@gmail.com>
2024-05-01 09:02:14 +0000

    added webhook for profile to complete purchase even if form submission disrupted

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from .models import Order, OrderLineItem
 from products.models import Product
-
+from profiles.models import UserProfile
 
 import json
 import time 
@@ -35,6 +35,22 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # update profile infro if save info is ticked
+
+        profile = None
+        username = intent.metadtaata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.filter.get(user__username=username)
+            if save_info:
+                phone_number = shipping_details.phone,
+                profile.default_country = shipping_details.address.country,
+                profile.default_postcode = shipping_details.address.postal_code,
+                profile.default_town_or_city = shipping_details.address.city,
+                profile.default_street_address1 = shipping_details.address.line1,
+                profile.default_street_address2 = shipping_details.address.line2,
+                profile.default_county = shipping_details.address.state,
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -67,6 +83,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
