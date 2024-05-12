973752ca0fe80648cfb7df8a510a5796bf2a997c
stephaniemaf <farrell.s.ava@gmail.com>
2024-05-12 13:16:28 +0000

    styling added for shopping cart in bag.py

--- babythingsshop/settings.py
+++ babythingsshop/settings.py
@@ -28,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['baby-things-shop-5ef2702b155d.herokuapp.com','8000-stephaniema-babythingss-scpb7i3lzwv.ws-eu111.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['baby-things-shop-5ef2702b155d.herokuapp.com','8000-stephaniema-babythingss-voz5g8viwo8.ws-eu111.gitpod.io', 'localhost']
 
 
 # Application definition
