c271add405555297b3f687e28db72d7178c4004c
Stephanie Farrell <farrell.s.ava@gmail.com>
2024-07-23 23:36:18 +0000

    rebuild

--- babythingsshop/settings.py
+++ babythingsshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
