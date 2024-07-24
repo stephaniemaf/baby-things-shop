230c432991a2b0efbad9c7bd950369da8ea199b8
Stephanie Farrell <farrell.s.ava@gmail.com>
2024-07-24 17:51:08 +0000

    added a runtime.txt file tostop heroku deploying the sie with python 12 as this was preventing smemails from sending, once i specified python 3.10.14 emails were sending

--- checkout/views.py
+++ checkout/views.py
@@ -203,7 +203,7 @@ def checkout(request):
 
 def checkout_success(request, order_number):
     """
-    Handle successful checkouts
+    Handle successful checkout
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
