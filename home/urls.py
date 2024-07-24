9b18191629d38dc7a368a5fb4f5794c3a0379729
Stephanie Farrell <farrell.s.ava@gmail.com>
2024-07-24 10:31:17 +0000

    rebuild

--- home/urls.py
+++ home/urls.py
@@ -2,11 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from views import send_test_email
+
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('sign_up/', views.sign_up, name='sign_up'),
     path('subscribe_newsletter/', views.subscribe_newsletter, name='subscribe_newsletter'),
-    path('send-test-email/', send_test_email, name='send_test_email'),
+    
 ]
